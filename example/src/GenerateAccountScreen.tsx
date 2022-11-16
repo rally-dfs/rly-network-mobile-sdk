db70aae724f452a8ec20f7f9338c50cf71a09f00
Ryan Weald <ryan@weald.com>
2022-11-15 16:56:22 -0800

    Cleanup UI for no account screen.

--- example/src/GenerateAccountScreen.tsx
+++ example/src/GenerateAccountScreen.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
-import { Text, Button } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 import { AppContainer } from './components/AppContainer';
+import { RlyCard } from './components/RlyCard';
+import { BodyText, HeadingText } from './components/text';
 
 export const GenerateAccountScreen = (props: {
   generateAccount: () => void;
 }) => {
   return (
     <AppContainer>
-      <Text>No Account Exists, You need to generate one</Text>
-      <Button title="Create RLY Account" onPress={props.generateAccount} />
+      <View>
+        <HeadingText>Welcome To The RLY Demo App</HeadingText>
+      </View>
+      <RlyCard style={styles.cardMargin}>
+        <View style={styles.marginBetween}>
+          <BodyText>Looks like you don't yet have an account</BodyText>
+        </View>
+        <Button title="Create RLY Account" onPress={props.generateAccount} />
+      </RlyCard>
     </AppContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  marginBetween: {
+    marginTop: 12,
+  },
+  cardMargin: {
+    marginTop: 24,
+  },
+});
