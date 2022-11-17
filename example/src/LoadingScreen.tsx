926fd9aecda1f0cfe90bf02c161b1c8cb09e1c3f
Ryan Weald <ryan@weald.com>
2022-11-16 16:47:56 -0800

    Slightly cleaner UX for initial loading screen

--- example/src/LoadingScreen.tsx
+++ example/src/LoadingScreen.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
-import { AppContainer } from './components/AppContainer';
-import { BodyText } from './components/text';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { BodyText, HeadingText } from './components/text';
 
 export const LoadingScreen = () => {
   return (
-    <AppContainer>
-      <BodyText> Loading RLY Account </BodyText>
-    </AppContainer>
+    <View style={styles.container}>
+      <HeadingText> Loading RLY Account </HeadingText>
+      <View style={styles.smallMargin}>
+        <BodyText>This may take several seconds</BodyText>
+      </View>
+      <ActivityIndicator style={styles.standardMargin} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 60,
+    paddingHorizontal: 12,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+  },
+  smallMargin: {
+    marginTop: 12,
+  },
+  standardMargin: {
+    marginTop: 24,
+  },
+});
