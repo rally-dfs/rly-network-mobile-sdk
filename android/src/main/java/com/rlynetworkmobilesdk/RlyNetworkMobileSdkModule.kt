e1ecd19e16a69fb1c3249255e6f978289c8cbb30
Cameron Morris <ctmorris@gmail.com>
2023-01-19 09:18:09 -0800

    Small changes to support Android (#7)
    * Add native support for getting bundle ID on Android.
    
    * Fix issues introduced by PR #6 
    
    * Gradle config to keep Android Studio happy.

--- android/src/main/java/com/rlynetworkmobilesdk/RlyNetworkMobileSdkModule.kt
+++ android/src/main/java/com/rlynetworkmobilesdk/RlyNetworkMobileSdkModule.kt
@@ -13,8 +13,13 @@ class RlyNetworkMobileSdkModule(reactContext: ReactApplicationContext) :
   }
 
   @ReactMethod
-  fun hello() {
+  fun hello(promise: Promise) {
     promise.resolve("Hello world")
+  }
+
+  @ReactMethod
+  fun getBundleId(promise:Promise){
+    promise.resolve(reactApplicationContext.packageName)
   }
 
   companion object {
