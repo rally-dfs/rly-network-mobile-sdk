a408dad125133b241e6825b7b3adb1534db3a165
botdad <91175836+botdad@users.noreply.github.com>
2023-02-15 16:43:40 -0800

    add warning around RNG

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,20 @@
+declare global {
+  var crypto: {
+    getRandomValues: (buffer: Uint8Array) => void;
+  };
+}
+
+const getRandomValues = (_buffer: Uint8Array) => {
+  throw new Error(
+    'The RNA SDK does not support the global.crypto.getRandomValues function and does all random number generation in native code. Proceed with caution'
+  );
+};
+
+if (global.crypto && !global.crypto.getRandomValues) {
+  global.crypto.getRandomValues = getRandomValues;
+} else if (!global.crypto) {
+  global.crypto = { getRandomValues };
+}
+
 export { getAccount, getAccountPhrase, createAccount } from './account';
 export * from './network';
