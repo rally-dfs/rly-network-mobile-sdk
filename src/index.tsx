9cc0f1c98fb620c32aea7896812a4551af5ce07f
botdad <91175836+botdad@users.noreply.github.com>
2023-02-15 21:05:23 -0800

    revert to including rn-get-random, it plays better with expo

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,5 @@
-declare global {
-  var crypto: {
-    getRandomValues: (buffer: Uint8Array) => void;
-  };
-}
-
-const getRandomValues = (_buffer: Uint8Array) => {
-  throw new Error(
-    'The RNA SDK does not support the global.crypto.getRandomValues function and does all random number generation in native code. Proceed with caution'
-  );
-};
-
-if (global.crypto && !global.crypto.getRandomValues) {
-  global.crypto.getRandomValues = getRandomValues;
-} else if (!global.crypto) {
-  global.crypto = { getRandomValues };
-}
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
 
 export { getAccount, getAccountPhrase, createAccount } from './account';
 export * from './network';
