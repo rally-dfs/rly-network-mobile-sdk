e1d7bd6551e79badb2f2926b0616825673d04821
dongambas <ctmorris@gmail.com>
2023-03-14 15:42:41 -0700

    remove external network dependency from tx sign test

--- src/__tests__/account.test.ts
+++ src/__tests__/account.test.ts
@@ -38,20 +38,12 @@ test('sign message', async () => {
 test('sign transaction', async () => {
   const wallet = await getWallet();
 
-  const provider = new providers.JsonRpcProvider(
-    MumbaiNetworkConfig.gsn.rpcUrl
-  );
-  const faucet = tokenFaucet(MumbaiNetworkConfig, provider);
-  const destinationAddress = Wallet.createRandom().address;
+  const testTx = {
+    data: '0xa9059cbb00000000000000000000000079b0ac67501aa89328f1154c749dfdb014232b05000000000000000000000000000000000000000000000000000000000000000a',
+    to: '0x946B1A4eA6457b285254Facb54B896Ab0fAE3a7C',
+  };
 
-  const tx = await faucet.populateTransaction.transfer?.(
-    destinationAddress,
-    10
-  );
-
-  if (!tx) throw new Error('tx is null');
-
-  const signedTx = await signTransaction(tx);
+  const signedTx = await signTransaction(testTx);
   const rawTx = utils.parseTransaction(signedTx);
   const { r, s, v } = rawTx;
 
@@ -60,9 +52,7 @@ test('sign transaction', async () => {
   const signature = utils.joinSignature({ r, s, v });
   expect(signature).toBeTruthy();
 
-  if (!tx) throw new Error('tx is null');
-
-  const raw = utils.serializeTransaction(tx);
+  const raw = utils.serializeTransaction(testTx);
   const msgHash = utils.keccak256(raw);
   const msgBytes = utils.arrayify(msgHash);
   const sender = utils.recoverAddress(msgBytes, signature);
