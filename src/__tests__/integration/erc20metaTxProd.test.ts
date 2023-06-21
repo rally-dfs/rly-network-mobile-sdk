d8c97c36b9356197a78eb417e820e53aadc02452
Jono Chang <jchang@rally.io>
2023-06-21 09:44:36 +0900

    skip some erc20metaTxProd tests in CI

--- src/__tests__/integration/erc20metaTxProd.test.ts
+++ src/__tests__/integration/erc20metaTxProd.test.ts
@@ -4,6 +4,7 @@ import { RlyPolygonNetwork } from '../../network';
 import { PolygonNetworkConfig } from '../../network_config/network_config_polygon';
 import * as ERC20 from '../../contracts/erc20Data.json';
 import { MetaTxMethod } from '../../gsnClient/utils';
+import { testSkipInCI } from '../__utils__/test_utils';
 
 let ethersProvider: ethers.providers.JsonRpcProvider;
 const tokenAddress = '0x76b8D57e5ac6afAc5D415a054453d1DD2c3C0094';
@@ -44,43 +45,53 @@ test('wallet', async () => {
   expect(wallet?.address).toEqual(address);
 });
 
-test('balance of polygon prod token should equal 10', async () => {
-  const account = await getWallet();
-  const signer = account?.connect(ethersProvider);
-  const token = new Contract(tokenAddress, ERC20.abi, signer);
-  const decimals = await token.decimals();
+// this can be marked with `testOnlyRunInCIFullSuite` instead if it's tweaked to be fully automated
+testSkipInCI(
+  'balance of polygon prod token should equal 10',
+  async () => {
+    const account = await getWallet();
+    const signer = account?.connect(ethersProvider);
+    const token = new Contract(tokenAddress, ERC20.abi, signer);
+    const decimals = await token.decimals();
 
-  if (!account) {
-    throw 'account not found';
-  }
-  const bal = await token.balanceOf(account.address);
-  expect(decimals).toEqual(18);
-  expect(bal).toEqual(ethers.utils.parseUnits('10', decimals));
-});
+    if (!account) {
+      throw 'account not found';
+    }
+    const bal = await token.balanceOf(account.address);
+    expect(decimals).toEqual(18);
+    expect(bal).toEqual(ethers.utils.parseUnits('10', decimals));
+  },
+  30000
+);
 
-test('transfer polygon prod token behind proxy using permit on our gsn client and paymaster with method argument', async () => {
-  const RlyNetwork = RlyPolygonNetwork;
-  const account = await getWallet();
-  const signer = account?.connect(ethersProvider);
-  const token = new Contract(tokenAddress, ERC20.abi, signer);
-  const decimals = await token.decimals();
+// this can be marked with `testOnlyRunInCIFullSuite` instead if it's tweaked to be fully automated
+testSkipInCI(
+  'transfer polygon prod token behind proxy using permit on our gsn client and paymaster with method argument',
+  async () => {
+    const RlyNetwork = RlyPolygonNetwork;
+    const account = await getWallet();
+    const signer = account?.connect(ethersProvider);
+    const token = new Contract(tokenAddress, ERC20.abi, signer);
+    const decimals = await token.decimals();
 
-  if (!account) {
-    throw 'account not found';
-  }
-  const oldBal = await token.balanceOf(account.address);
+    if (!account) {
+      throw 'account not found';
+    }
+    const oldBal = await token.balanceOf(account.address);
 
-  // ant address
-  const to = '0xc073ade46aba2f72bf27e7befd37af9301cd8920';
-  const txHash = await RlyNetwork.transfer(
-    to,
-    1,
-    tokenAddress,
-    MetaTxMethod.ExecuteMetaTransaction
-  );
+    // ant address
+    const to = '0xc073ade46aba2f72bf27e7befd37af9301cd8920';
+    const txHash = await RlyNetwork.transfer(
+      to,
+      1,
+      tokenAddress,
+      MetaTxMethod.ExecuteMetaTransaction
+    );
 
-  const newBal = await token.balanceOf(account.address);
-  expect(oldBal).toEqual(ethers.utils.parseUnits('10', decimals));
-  expect(txHash).toMatch(/^0x/);
-  expect(newBal).toEqual(ethers.utils.parseUnits('9', decimals));
-});
+    const newBal = await token.balanceOf(account.address);
+    expect(oldBal).toEqual(ethers.utils.parseUnits('10', decimals));
+    expect(txHash).toMatch(/^0x/);
+    expect(newBal).toEqual(ethers.utils.parseUnits('9', decimals));
+  },
+  30000
+);
