4f8dc4fecda3f5a2147c8799c81289a965d0c03a
dongambas <ctmorris@gmail.com>
2023-08-18 10:16:34 -0700

    update account exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,6 @@
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
 
-export {
-  getAccount,
-  getAccountPhrase,
-  createAccount,
-  signMessage,
-  signTransaction,
-  signHash,
-  signTypedData,
-  permanentlyDeleteAccount,
-} from './account';
+export * from './account';
 export { MetaTxMethod } from './gsnClient/utils';
 export * from './network';
