c800cccde8f27531f188efa792ee3c99a2c8c246
Ryan Weald <ryan@weald.com>
2024-10-14 15:08:28 -0700

    Add pre-built token config for 2 different RLY tokens on Sepolia base
    Also backfilled some documentation for this new token setup

--- src/transactions/supported_tokens.ts
+++ src/transactions/supported_tokens.ts
@@ -1,16 +1,49 @@
 import type { EIP712Domain } from '../gsnClient/EIP712/typedSigning';
 import { MetaTxMethod } from '../gsnClient/utils';
 
+/*
+ * TokenConfig is a configuration object that is used to define the properties of a token.
+ * address: The address of the token contract.
+ * metaTxnMethod: The method of meta transaction that the token supports. See MetaTxMethod for more information.
+ * This is most likely going to be MetaTxMethod.Permit.
+ * eip712Domain: The EIP712 domain object for the token. This is only required if the token uses non default values for EIP712 signature generation.
+ */
 export interface TokenConfig {
   address: string;
   metaTxnMethod: MetaTxMethod;
   eip712Domain?: EIP712Domain;
 }
 
+/*
+ * Pre built configuration for USDC on Base Mainnet
+ */
 export const BaseUSDC: TokenConfig = {
   address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
   metaTxnMethod: MetaTxMethod.Permit,
   eip712Domain: {
     version: '2',
   },
+};
+
+/*
+ * Pre built configuration for RLY on Sepolia Base.
+ * This is the token this SDK tests with by default.
+ */
+export const BaseSepoliaRLY: TokenConfig = {
+  address: '0x846D8a5fb8a003b431b67115f809a9B9FFFe5012',
+  metaTxnMethod: MetaTxMethod.Permit,
+  eip712Domain: {
+    version: '1',
+  },
+};
+
+/*
+ * This is a custom version of RLY configured to support
+ * the executeMetaTransaction style of meta transactions.
+ * Should only be used for specific testing purposes. If you aren't sure
+ * whether you need MetaTxMethod.ExecuteMetaTransaction, you probably don't.
+ */
+export const BaseSepoliaExecMetaRLY: TokenConfig = {
+  address: '0x16723e9bb894EfC09449994eC5bCF5b41EE0D9b2',
+  metaTxnMethod: MetaTxMethod.ExecuteMetaTransaction,
 };
